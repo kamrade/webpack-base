31364e88684dbe169c22c5cfc6db92422bad26a1
Denis Michailov <kamrad.reg@gmail.com>
2017-08-16 21:56:49 +1000

    add pug support

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-
   entry: {
     app: './src/index.js',
     print: './src/print.js'
@@ -19,7 +18,7 @@ module.exports = {
     // new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Output Management',
-      template: './src/index.html'
+      template: 'src/index.pug'
     })
   ],
 
@@ -31,50 +30,46 @@ module.exports = {
 
   module: {
     rules: [
-
       {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+          pretty: true
+        }
+      },{
         test: /\.css$/,
         use: [
           'style-loader',
           'css-loader'
         ]
-      },
-      {
+      },{
         test: /\.(scss|sass)$/,
-        use: [{
-          loader: 'style-loader'
-        }, {
-          loader: 'css-loader'
-        }, {
-          loader: 'sass-loader'
-        }]
-      },
-      {
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'},
+          {loader: 'sass-loader'}
+        ]
+      },{
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           'file-loader'
         ]
-      },
-      {
+      },{
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
           'file-loader'
         ]
-      },
-      {
+      },{
         test: /\.(csv|tsv)$/,
         use: [
           'csv-loader'
         ]
-      },
-      {
+      },{
         test: /\.xml$/,
         use: [
           'xml-loader'
         ]
       }
-
     ]
   }
-
 };
