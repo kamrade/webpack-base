f9551e793476069ac0b56bcfaa0ff93d5d4031d1
Denis Mikhailov <denis.michailov@gmail.com>
2017-08-18 17:08:02 +1000

    Clean code

--- src/index.js
+++ src/index.js
@@ -2,57 +2,29 @@
 import printMe from './print.js';
 import './style.scss';
 import template from './view/header.pug';
-import $ from 'jquery';
 
+import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  Router,
-  Route,
-  IndexRoute,
-  Link,
-  Navigation,
-  browserHistory,
-  hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, Link, Navigation, browserHistory, hashHistory } from 'react-router';
 
-// templates ------------------------------------
 
-function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
 
-  element.innerHTML = 'Hello, webpack <br>';
-  element.classList.add('hello');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element;
-}
-
-var element = component(); // Store the elementto re-render on print.js changes
-document.body.appendChild(element);
-
+// render template from jade file
 var appContainer = document.createElement('div');
 appContainer.innerHTML = template();
 appContainer.classList.add('app-container');
 document.body.appendChild(appContainer);
 
+// create react container
 var reactApp = document.createElement('div');
 reactApp.id = "app";
 reactApp.classList.add('app');
 document.body.appendChild(reactApp);
 
-// test jquery
-let $hello = $(".hello");
-$hello.css("background", "#333");
-$hello.css("color", "#fff");
-
 // react ------------------------------------
 
 ReactDOM.render(
-  <h2> Hello, world Again</h2>,
+  <h2> Hello, from react</h2>,
   document.getElementById('app')
 );
