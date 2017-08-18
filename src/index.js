73260454aee1be0f03d7c51e0205a385ff130689
Denis Mikhailov <denis.michailov@gmail.com>
2017-08-18 18:01:34 +1000

    add d3

--- src/index.js
+++ src/index.js
@@ -3,26 +3,9 @@ import printMe from './print.js';
 import './style.scss';
 import template from './view/header.pug'
 
-function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
-
-  element.innerHTML = 'Hello, webpack <br>';
-  element.classList.add('hello');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element;
-}
-
-var element = component(); // Store the elementto re-render on print.js changes
-document.body.appendChild(element);
-
 var appContainer = document.createElement('div');
 appContainer.innerHTML = template();
-appContainer.classList.add('app-container');
+appContainer.id = "app";
+appContainer.classList.add('app');
 
 document.body.appendChild(appContainer);
